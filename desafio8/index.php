bd5678dba99bc14c4f245c3151da97f310444444
Shenrique <79231553+shenrique1970@users.noreply.github.com>
2025-10-22 09:44:08 -0300

    desafio 8 raizes

---
+++ desafio8/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Desafio 8</title>
+    <link rel="stylesheet" href="../css/estilo.css">
+</head>
+<body>
+    <header>
+        <h1>Raízes</h1>
+    </header>
+    <main>
+        <form action="<?= $_SERVER['PHP_SELF'] ?>" method="post">
+            <label for="numero">Informe o número?</label>
+            <input type="number" name="numero">
+            <input type="submit" value="Enviar">
+        </form>
+    </main>
+    <section>
+        <h2>Resultado final</h2>
+        <?php 
+            // https://www.php.net/manual/pt_BR/function.sqrt.php
+            // https://www.php.net/manual/pt_BR/function.pow.php
+            // https://www.php.net/manual/en/function.number-format.php
+            $numero = (float) $_POST["numero"];
+            echo "Analisando o número " . $numero . "<br>";
+            $raiz = sqrt($numero);
+            echo "A raiz quadrada de " . $numero . " é igual a " . number_format($raiz,3,",",".") . "<br>";
+            $cubica = pow($numero, 1/3);
+            echo "A raiz cúbica de " . $numero . " é igual a " . number_format($cubica,3,",",".") . "<br>";
+        ?>  
+    </section>
+</body>
+</html>
