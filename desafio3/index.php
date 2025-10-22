a0a78d76387f5549ab52777b00e5ceaa96892585
Shenrique <79231553+shenrique1970@users.noreply.github.com>
2025-10-22 09:28:13 -0300

    desafio 3 Conversor de moedas.

---
+++ desafio3/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Desafio 3</title>
+    <link rel="stylesheet" href="../css/estilo.css">
+</head>
+<body>
+    <header>
+        <h1>Conversor de moedas.</h1>
+    </header>
+    <section>
+        <form action="conv.php" method="get">
+            <label for="valor">Quanto vai converter?</label>
+            <input type="number" name="valor" id="idvalor">
+            <input type="submit" value="Enviar">
+        </form>
+        
+    </section>
+    
+</body>
+</html>
