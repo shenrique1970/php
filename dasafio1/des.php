e32d8acdfb441595b1d181517051de6d844b7797
Shenrique <79231553+shenrique1970@users.noreply.github.com>
2025-10-22 09:17:29 -0300

    desafio 1 Antecessor e sucessor.

---
+++ dasafio1/des.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Desafio 1</title>
+    <link rel="stylesheet" href="../css/estilo.css">
+</head>
+
+<body>
+    <header>
+        <h1>Desafio 1</h1>
+    </header>
+    <main>
+        <h3>Antecessor e sucessor.</h3>
+        <?php
+        // isset() para evitar erro
+        if (isset($_GET["numero"])) {
+            $numero = (int) $_GET["numero"];
+            echo "Número digitado: " . $numero . "<br>";
+            echo "Número anterior: " . ($numero - 1) . "<br>";
+            echo "Número posterior: " . ($numero + 1) . "<br>";
+        }
+        ?>
+        <button onclick="javascript:history.go(-1)">Voltar</button>
+    </main>
+</body>
+
+</html>
