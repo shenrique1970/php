0186f264a0c2fa91290621075e8e79569226e8a3
Shenrique <79231553+shenrique1970@users.noreply.github.com>
2025-10-22 09:42:09 -0300

    desafio 7 Salário

---
+++ desafio7/index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Desafio 7</title>
+    <link rel="stylesheet" href="../css/estilo.css">
+</head>
+<body>
+    <header>
+        <h1>Salário</h1>
+    </header>
+    <main>
+        <form action="<?= $_SERVER['PHP_SELF'] ?>" method="post">
+            <label for="renda">Informe o salário?</label>
+            <input type="number" name="renda" step="0.01">
+            <label for="salario">Informe o salário minimo vigente?</label>
+            <input type="number" name="salario" step="0.01">
+            
+            <input type="submit" value="Enviar">
+        </form>
+    </main>
+    <section>
+        <h2>Resultado final</h2>
+        <?php 
+            $renda = (int) $_POST["renda"] ?? 0;
+            // informe do valora do salário vigente.
+            $salario = (int) $_POST["salario"];
+            echo "<span>Considerando o salário minimo de <b>R\$ " . number_format($salario, 2, ",", ".") . "</b></span><br>";
+            // não pode ser número quebrado ou divisão real. usar divisão inteira.
+            $quant = intdiv($renda, $salario); // ou usar (int) ($renda / $salario);
+            $dif = $renda % $salario;    // o resto da divião é a diferença de salário.
+            echo "Quem recebe R\$ " . number_format($renda,1,",",".") . " recebe  " . intval(number_format($quant,1,",",".")) . " salário(s) mínimo(s)." ;
+            echo " Mais a diferença de R\$ " . number_format($dif,1,",",".") . "<br>";
+        ?>    
+    </section>
+</body>
+</html>
