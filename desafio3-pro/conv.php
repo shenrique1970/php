1bc816cc6c4525998b60a5b3edd6e8c15d54ee2b
Shenrique <79231553+shenrique1970@users.noreply.github.com>
2025-10-23 11:59:25 -0300

    novas correções desafio 3 pro

--- desafio3-pro/conv.php
+++ desafio3-pro/conv.php
@@ -14,15 +14,21 @@
     </header>
     <?php 
         $real = $_GET["valor"];
-        $cotacao = 5.17;
-        $dolar = $real / $cotacao;
     ?>
     <main>
         <?php
+            $cotacao = 5.17;
+            $dolar = $real / $cotacao;
             $padrao = numfmt_create("pt_BR", NumberFormatter::CURRENCY);
-
-            echo "Seus " . numfmt_format_currency($padrao, $real, "BRL") . " equivalem a  " .  numfmt_format_currency($padrao, $dolar, "USD");
+            $padReal = numfmt_format_currency($padrao, $real, "BRL");
+            $padDolar = numfmt_format_currency($padrao, $dolar, "USD");
         ?>
+        <section>
+            <h2>Detalhes da conversão</h2>
+            <p>Valor convertido: <?php echo $padReal; ?></p>
+            <p>Cotação utilizada: <?php echo numfmt_format_currency($padrao, $cotacao, "BRL"); ?></p>
+            <p>Valor final em Dólares: <?php echo $padDolar; ?></p>
+        </section>
         <button onclick="javascript:history.go(-1)">Voltar</button>
     </main>
 
