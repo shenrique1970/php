9e90f8908a5a5a6bc2e54dcc81b94ffbdb34d64c
Shenrique <79231553+shenrique1970@users.noreply.github.com>
2025-10-23 12:41:47 -0300

    correções desafio 7

--- desafio7/index.php
+++ desafio7/index.php
@@ -1,13 +1,11 @@
 <!DOCTYPE html>
 <html lang="pt-br">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Desafio 7</title>
     <link rel="stylesheet" href="../css/estilo.css">
 </head>
-
 <body>
     <header>
         <h1>Salário</h1>
@@ -16,37 +14,32 @@
         <!--$_SERVER['PHP_SELF'] envia o formulario pra ele mesmo -->
         <form action="<?= htmlspecialchars($_SERVER['PHP_SELF']) ?>" method="post">
             <label for="renda">Informe o salário?</label>
-            <input type="number" name="renda" step="0.01" required>
+            <input type="number" name="renda" step="0.01">
             <label for="salario">Informe o salário minimo vigente?</label>
-            <input type="number" name="salario" step="0.01" required>
-
+            <input type="number" name="salario" step="0.01">
+            
             <input type="submit" value="Enviar">
         </form>
     </main>
-    <?php
-        $renda = isset($_POST["renda"]) ? (float) $_POST["renda"] : 0;
-        $sal = isset($_POST["salario"]) ? (float) $_POST["salario"] : 0;
+    <?php 
+        $renda = (int) $_POST["renda"] ?? 0;
+        $sal = (int) $_POST["salario"];
     ?>
     <section>
         <h2>Resultado final</h2>
-        <?php if ($_SERVER["REQUEST_METHOD"] == "POST" && $sal > 0): ?>
-            <?php
-            $quant = (int) ($renda / $sal);
-            $dif = $renda - ($quant * $sal);
-            ?>
-            <section>
-                <h2>Resultado final</h2>
-                <article>
-                    <span>Considerando o salário mínimo de <b>R$ <?= number_format($sal, 2, ",", ".") ?></b></span><br>
-                    <span>Quem recebe R$ <?= number_format($renda, 2, ",", ".") ?> recebe <?= $quant ?> salário(s) mínimo(s) mais a diferença de R$ <?= number_format($dif, 2, ",", ".") ?></span>
-                </article>
-            </section>
-        <?php else: ?>
-            <section>
-                <h2>Resultado final</h2>
-                <span>Aguardando o envio dos dados...</span>
-            </section>
-        <?php endif; ?>
+        <?php 
+            $quant = intdiv($renda, $sal); // ou usar (int) ($renda / $sal);
+            $dif = $renda % $sal;    // o resto da divião é a diferença de salário.
+            
+            if (!$_POST) {
+                echo "<span>Aguardando o envio dos dados...</span>";
+                return;
+            }    
+        ?>  
+        <article>
+           <span>Considerando o salário minimo de <b>R$ <?= number_format($sal, 2, ",", ".") ?></b></span>
+            <span>Quem recebe R$ <?= number_format($renda, 2, ",", ".") ?> recebe <?= $quant ?> salário(s) mínimo(s) mais a diferença de R$ <?= number_format($dif, 2, ",", ".") ?></span> 
+        </article>  
+    </section>
 </body>
-
 </html>
