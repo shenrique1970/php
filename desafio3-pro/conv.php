e560370e5ebee486b72a865609f6af48e080574d
Shenrique <79231553+shenrique1970@users.noreply.github.com>
2025-10-23 11:52:57 -0300

    correções desafio3 pro

--- desafio3-pro/conv.php
+++ desafio3-pro/conv.php
@@ -12,21 +12,16 @@
     <header>
         <h1>Conversor de moedas.</h1>
     </header>
+    <?php 
+        $real = $_GET["valor"];
+        $cotacao = 5.17;
+        $dolar = $real / $cotacao;
+    ?>
     <main>
         <?php
-        // valor do dolar $_GET["cotacao"];
-        $cotacao = 5.17;
-        // busca valor digitado
-        $real = $_GET["valor"];
-        // valor em dolar
-        $dolar = $real / $cotacao;
-        // internacionalização da moeda intl
-        // $padrao = numfmt_create("us", NumberFormatter::CURRENCY);
-        // $padrao = numfmt_create("pt_PT", NumberFormatter::CURRENCY);
-        // $padrao = numfmt_create("ur_RU", NumberFormatter::CURRENCY);
-        $padrao = numfmt_create("pt_BR", NumberFormatter::CURRENCY);
+            $padrao = numfmt_create("pt_BR", NumberFormatter::CURRENCY);
 
-        echo "Seus " . numfmt_format_currency($padrao, $real, "BRL") . " equivalem a  " .  numfmt_format_currency($padrao, $dolar, "USD");
+            echo "Seus " . numfmt_format_currency($padrao, $real, "BRL") . " equivalem a  " .  numfmt_format_currency($padrao, $dolar, "USD");
         ?>
         <button onclick="javascript:history.go(-1)">Voltar</button>
     </main>
