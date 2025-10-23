fb8e242dd6b75d24477ff08824d3c381de4cd49d
Shenrique <79231553+shenrique1970@users.noreply.github.com>
2025-10-23 12:08:52 -0300

    correções desafio 4 api

--- desafio4-api/conv.php
+++ desafio4-api/conv.php
@@ -12,34 +12,32 @@
     <header>
         <h1>Conversor de moedas.</h1>
     </header>
-    <main>
-        <?php
-        // valor do dolar $_GET["cotacao"];
-        // o dia dehoje menos 7 dias em php
-        $inicio = date("m-d-Y", strtotime("-7 days"));
-        // data atual
-        $fim = date("m-d-Y");
-        // 
-        $url = 'https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoDolarPeriodo(dataInicial=@dataInicial,dataFinalCotacao=@dataFinalCotacao)?@dataInicial=\''. $inicio .'\'&@dataFinalCotacao=\''.$fim .'\'&$top=1&$orderby=dataHoraCotacao%20desc&$format=json&$select=cotacaoCompra,dataHoraCotacao';
-
-        $dados = json_decode(file_get_contents($url), true);
-        //var_dump($dados);
-        $cotacao = $dados["value"][0]["cotacaoCompra"];
-        
+    <?php 
         // busca valor digitado
         $real = $_GET["valor"];
-        // valor em dolar
-        $dolar = $real / $cotacao;
-        // internacionalização da moeda intl
-        // $padrao = numfmt_create("us", NumberFormatter::CURRENCY);
-        // $padrao = numfmt_create("pt_PT", NumberFormatter::CURRENCY);
-        // $padrao = numfmt_create("ur_RU", NumberFormatter::CURRENCY);
-        $padrao = numfmt_create("pt_BR", NumberFormatter::CURRENCY);
-        echo "<section><pre>";
-        echo "A cotação foi de " . numfmt_format_currency($padrao, $cotacao, "USD") . "<br>";
-        echo "Seus " . numfmt_format_currency($padrao, $real, "BRL") . "<br>" . "equivalem a  " .  numfmt_format_currency($padrao, $dolar, "USD");
-        echo "</pre></section>";
+    ?>
+    <main>
+        <?php 
+            $inicio = date("m-d-Y", strtotime("-7 days"));
+            $fim = date("m-d-Y");
+            $url = 'https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoDolarPeriodo(dataInicial=@dataInicial,dataFinalCotacao=@dataFinalCotacao)?@dataInicial=\''. $inicio .'\'&@dataFinalCotacao=\''.$fim .'\'&$top=1&$orderby=dataHoraCotacao%20desc&$format=json&$select=cotacaoCompra,dataHoraCotacao';
+            $dados = json_decode(file_get_contents($url), true);
+            //var_dump($dados);
+            $cotacao = $dados["value"][0]["cotacaoCompra"];
+            // valor em dolar
+            $dolar = $real / $cotacao;
+            $padrao = numfmt_create("pt_BR", NumberFormatter::CURRENCY);
         ?>
+        <section>
+            <h2>Resultado da conversão</h2>
+            <article>
+            <?php
+            echo "A cotação foi de " . numfmt_format_currency($padrao, $cotacao, "USD") . "<br>";
+            echo "Seus " . numfmt_format_currency($padrao, $real, "BRL") . "<br>" . "equivalem a  " .  numfmt_format_currency($padrao, $dolar, "USD");
+            ?>
+            </article>
+        </section>
+        
         <button onclick="javascript:history.go(-1)">Voltar</button>
     </main>
 
