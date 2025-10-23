6209ea43f4f0f5f9008621a47fd1797930cf1dcc
Shenrique <79231553+shenrique1970@users.noreply.github.com>
2025-10-23 08:11:08 -0300

    correções

--- desafio11/index.php
+++ desafio11/index.php
@@ -14,26 +14,33 @@
     </header>
     <!--https://www.w3schools.com/tags/att_input_type_range.asp-->
     <main>
-        <form action="<?= $_SERVER['PHP_SELF'] ?>" method="post">
+        <!--htmlspecialchars() para evitar vulnerabilidades XSS:
+        é uma boa prática de segurança para proteger seu formulário 
+        contra injeções de código malicioso, mantendo o envio na mesma página.
+        -->
+        <form action="<?= htmlspecialchars($_SERVER['PHP_SELF']) ?>" method="post">
             <label for="numero">Preço do produto</label>
-            <input type="number" name="preco" step="0.01">
+            <input type="number" id="preco" name="preco" step="0.01" required>
             <label for="volume">Qual será o percentual de ajuste?</label>
             <input type="range" id="volume" name="volume" min="0" max="100" value="0" oninput="this.nextElementSibling.value = this.value + '%'">
-            <output>0%</output>
+            <output><?= $_POST['volume'] ?? 0 ?>%</output>
             <input type="submit" value="Enviar">
         </form>
     </main>
     <article>
-        <?php
-        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        <?php if ($_SERVER["REQUEST_METHOD"] == "POST"): ?>
+            <?php
             $preco = (float) $_POST["preco"];
             $percentual = (float) $_POST["volume"];
             $valorAjuste = $preco * ($percentual / 100);
             $precoAjustado = $preco + $valorAjuste;
-
-            echo "O produto que custava R$ " . number_format($preco, 2, ",", ".") . ", com o reajuste de " . number_format($percentual, 2, ",", ".") . "%, vai passar a custar R$ " . number_format($precoAjustado, 2, ",", ".") . ".";
-        }
-        ?>
+            ?>
+            <p>
+                O produto que custava <strong>R$ <?= number_format($preco, 2, ",", ".") ?></strong>,
+                com o reajuste de <strong><?= number_format($percentual, 0, ",", ".") ?>%</strong>,
+                vai passar a custar <strong>R$ <?= number_format($precoAjustado, 2, ",", ".") ?></strong>.
+            </p>
+        <?php endif; ?>
     </article>
 </body>
 
