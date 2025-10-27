7875e9347be9b7adeb5679bf85c664dc9c0abfbe
Shenrique <79231553+shenrique1970@users.noreply.github.com>
2025-10-27 09:45:08 -0300

    correção desafio 7

--- desafio7/index.php
+++ desafio7/index.php
@@ -1,45 +1,59 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Desafio 7</title>
     <link rel="stylesheet" href="../css/estilo.css">
 </head>
+
 <body>
     <header>
         <h1>Salário</h1>
     </header>
+    <?php
+    // Inicializa todas as variáveis com valor zero
+    $renda = $salario = $quant = $dif = $resultado = 0;
+    $renda = $_POST["renda"] ?? 1;
+    $salario = $_POST["salario"] ?? 1;
+    ?>
     <main>
         <!--$_SERVER['PHP_SELF'] envia o formulario pra ele mesmo -->
         <form action="<?= htmlspecialchars($_SERVER['PHP_SELF']) ?>" method="post">
             <label for="renda">Informe o salário?</label>
-            <input type="number" name="renda" step="0.01">
+            <input type="number" name="renda" step="0.01" min="1">
             <label for="salario">Informe o salário minimo vigente?</label>
-            <input type="number" name="salario" step="0.01">
-            
+            <input type="number" name="salario" step="0.01" min="1">
+
             <input type="submit" value="Enviar">
         </form>
     </main>
-    <?php 
-        $renda = (int) $_POST["renda"] ?? 0;
-        $sal = (int) $_POST["salario"];
-    ?>
+
     <section>
         <h2>Resultado final</h2>
-        <?php 
-            $quant = intdiv($renda, $sal); // ou usar (int) ($renda / $sal);
-            $dif = $renda % $sal;    // o resto da divião é a diferença de salário.
-            
-            if (!$_POST) {
-                echo "<span>Aguardando o envio dos dados...</span>";
-                return;
-            }    
-        ?>  
+        <?php
+        // https://www.php.net/manual/pt_BR/function.intdiv.php
+        // https://www.php.net/manual/en/function.number-format.php
+        // Evita divisão por zero
+        if ($salario !== 0) {
+            $resultado = intdiv($renda, $salario);
+        } else {
+            echo "Erro: divisão por zero não é permitida.";
+        }
+
+        $dif = $renda % $salario;    // o resto da divião é a diferença de salário.
+
+        if (!$_POST) {
+            echo "<span>Aguardando o envio dos dados...</span>";
+            return;
+        }
+        ?>
         <article>
-           <span>Considerando o salário minimo de <b>R$ <?= number_format($sal, 2, ",", ".") ?></b></span>
-            <span>Quem recebe R$ <?= number_format($renda, 2, ",", ".") ?> recebe <?= $quant ?> salário(s) mínimo(s) mais a diferença de R$ <?= number_format($dif, 2, ",", ".") ?></span> 
-        </article>  
+            <span>Considerando o salário minimo de <b>R$ <?= number_format($salario, 2, ",", ".") ?></b></span>
+            <span>Quem recebe R$ <?= number_format($renda, 2, ",", ".") ?> recebe <?= $resultado ?> salário(s) mínimo(s) mais a diferença de R$ <?= number_format($dif, 2, ",", ".") ?></span>
+        </article>
     </section>
 </body>
+
 </html>
