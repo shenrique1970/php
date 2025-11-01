726fe96914d9a27703a6d0e0a90836a5cf5c8aa3
Shenrique <79231553+shenrique1970@users.noreply.github.com>
2025-11-01 09:47:19 -0300

    mais exemplos interface

--- interface/outro.php
+++ interface/outro.php
@@ -29,3 +29,37 @@ $meuCirculo = new Circulo(5); // raio = 5
 // Usando os métodos definidos pela interface
 echo "Área do círculo: " . $meuCirculo->calcularArea() . "<br>";
 echo "Perímetro do círculo: " . $meuCirculo->calcularPerimetro();
+
+// outro exemplo
+class Retangulo implements FormaGeometrica {
+    private float $largura;
+    private float $altura;
+
+    public function __construct(float $largura, float $altura) {
+        $this->largura = $largura;
+        $this->altura = $altura;
+    }
+
+    public function calcularArea(): float {
+        return $this->largura * $this->altura;
+    }
+
+    public function calcularPerimetro(): float {
+        return 2 * ($this->largura + $this->altura);
+    }
+}
+
+// polimorfismo
+function mostrarForma(FormaGeometrica $forma) {
+    echo "Área: " . $forma->calcularArea() . "<br>";
+    echo "Perímetro: " . $forma->calcularPerimetro() . "<br><br>";
+}
+
+$formas = [
+    new Circulo(3),
+    new Retangulo(4, 6)
+];
+
+foreach ($formas as $forma) {
+    mostrarForma($forma);
+}
